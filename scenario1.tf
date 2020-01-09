4e0b332950bf270d578d50e309fc836242275bb5
Stu Cunliffe <s_cunliffe@uk.ibm.com>
2020-01-09 16:25:38 +0000

    Update scenario1.tf

--- scenario1.tf
+++ scenario1.tf
@@ -47,3 +47,6 @@ resource "ibm_compute_vm_instance" "debian_small_virtual_guest" {
   tags                     = ["${module.camtags.tagslist}"]
 }
 
+output "vm_ip" {
+  value = "Public : ${ibm_compute_vm_instance.debian_small_virtual_guest.ipv4_address}"
+}
